c326d37bfb2b10c87111d06fcc5ac41f1dcdeb54
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-03-11 22:33:43 -0800

    update user routes and thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const userRoutes = require('./user-routes');
 
 router.use('/thoughts', thoughtRoutes);
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
